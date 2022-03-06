6a2a202ccaac4ed7477613140b81f0fb78272107
Damian Ferro <dfmartinez8909@gmail.com>
2022-03-06 16:08:12 +0100

    bootstrap and nodemon added

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -1,7 +1,7 @@
 /* GET home page */
 
 module.exports.index = (req, res) => {
-    res.render('index', { title: 'Express Main' });
+    res.render('index', { title: 'Express' });
   };
   
   module.exports.test = (req, res) => {
